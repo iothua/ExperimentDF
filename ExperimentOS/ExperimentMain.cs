6ed060054170d6236acfb2800b77bdcc480e65c5
成华请 <1158078383@qq.com>
2021-01-07 17:45:43 +0800

    提交初始资源

---
+++ ExperimentOS/ExperimentMain.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Autofac;
+
+namespace ExperimentOS
+{
+    public class ExperimentMain
+    {
+        
+    }
+    
+}
---
+++ ExperimentOS/Program.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExperimentOS
+{
+    class Program
+    {
+        private static IContainer Container { get; set; }
+
+        static void Main(string[] args)
+        { 
+            
+        }
+
+        public static void WriteDate()
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            { 
+                
+            }
+        }
+    }
+}
